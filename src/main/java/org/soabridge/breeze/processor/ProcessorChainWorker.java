7ab5e4afbe5946a00561197b3168c8434c062496
kimba74 <slkrause08@gmail.com>
2014-09-01 18:00:00 -0400

    Created skeleton for Processor chain handling

--- src/main/java/org/soabridge/breeze/processor/ProcessorChainWorker.java
+++ src/main/java/org/soabridge/breeze/processor/ProcessorChainWorker.java
@@ -1,5 +1,7 @@
 package org.soabridge.breeze.processor;
 
+import org.soabridge.breeze.messaging.Message;
+
 import java.util.logging.Logger;
 
 /**
@@ -10,18 +12,30 @@ public class ProcessorChainWorker implements Runnable {
 
     private static Logger logger = Logger.getLogger(ProcessorChainWorker.class.toGenericString());
 
-    private String message;
+    private Message message;
 
 
-    public ProcessorChainWorker() {
+    public ProcessorChainWorker(Message message) {
+        this.message = message;
     }
 
     @Override
     public void run() {
-    }
+        try {
+            logger.finer("Starting to process message " + message.getId());
+            // TODO: Read current Stage and Stage Pipeline from Message
+            Class<? extends Processor>[] processes = new Class[0];
 
-    public synchronized void processMessage(String message) {
-        this.message = message;
-        this.notify();
+            // TODO: Build ProcessorHandler chain
+            ProcessorHandler handler = new ProcessorHandler(processes);
+
+            // TODO: Tell ProcessorHandler to process Message
+            Message result = handler.process(message);
+
+            // TODO: Handle processing output
+        }
+        catch (ProcessorException | ProcessingException e) {
+            // TODO: Handle failed processing (Idea: Have HiveFaultHandler)
+        }
     }
 }
