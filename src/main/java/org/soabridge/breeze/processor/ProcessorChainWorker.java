85aa988e1eb651a0a1797a192df0096fb21eac3a
kimba74 <slkrause08@gmail.com>
2014-09-02 21:14:45 -0400

    Added some more TODO information to the Chain Worker

--- src/main/java/org/soabridge/breeze/processor/ProcessorChainWorker.java
+++ src/main/java/org/soabridge/breeze/processor/ProcessorChainWorker.java
@@ -33,6 +33,12 @@ public class ProcessorChainWorker implements Runnable {
             Message result = handler.process(message);
 
             // TODO: Handle processing output
+            //     * Determine the next processing Stage from the state of the message
+            //     * If the next Stage is the 'Solution' Stage retrieve the messages from the docket and
+            //       create a chain worker for every message in the docket
+            //     * If the next Stage is the 'Post-Processing' Stage there should only be one message available.
+            //       Create a chain worker and pass the message to it.
+            //     * Should there be no more stage deliver the message to its designated Connector Endpoint
         }
         catch (ProcessorException | ProcessingException e) {
             // TODO: Handle failed processing (Idea: Have HiveFaultHandler)
