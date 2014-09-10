0b922d82ec6dd95298d537e3268dfa744c57b1b2
deevallabh <divyesh.vallabh@soabridge.com>
2014-09-10 17:20:38 -0400

    added methods to retrieve first, next and last phase

--- src/main/java/org/soabridge/breeze/messaging/DefaultActionPipelineConfiguration.java
+++ src/main/java/org/soabridge/breeze/messaging/DefaultActionPipelineConfiguration.java
@@ -1,6 +1,9 @@
 package org.soabridge.breeze.messaging;
 
-import java.util.HashSet;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -20,24 +23,57 @@ public class DefaultActionPipelineConfiguration implements ActionPipelineConfigu
         PHASE3;
     }
 
-    private Set<String> pipleline;
+    private List<String> pipleline;
 
     /**
      * Sets the default phases for breeze
      */
     public DefaultActionPipelineConfiguration()
     {
-        pipleline = new HashSet<String>();
+        pipleline = new ArrayList<String>();
         for (Pipleline phase : Pipleline.values())
             pipleline.add(phase.toString());
     }
+
+    @Override
+    public String getFirstPipeline() {
+        return Pipleline.PHASE1.toString();
+    }
+
+    @Override
+    public String getLastPipeline() {
+        return Pipleline.PHASE3.toString();
+    }
+
+    @Override
+    public String getNextPipeline(String currentPipeline) {
+
+        String nextPipeline = null;
+
+        //if empty, then regturn the first value in the pipeline
+        if (StringUtils.isBlank(currentPipeline))
+            nextPipeline =  Pipleline.PHASE1.toString();
+        else if (Pipleline.PHASE1.name().equals(currentPipeline))
+            nextPipeline = Pipleline.PHASE2.toString();
+        else if (Pipleline.PHASE2.name().equals(currentPipeline))
+            nextPipeline = Pipleline.PHASE3.toString();
+        else if (Pipleline.PHASE3.name().equals(currentPipeline))
+            nextPipeline = null;
+        else
+            throw new IllegalArgumentException("Cannot determine next phase in pipeline");
+
+        return nextPipeline;
+    }
+
 
     /**
      * Gets the list of pipe lines to be configured
      * @return Set<String> : pipe line to be configured
      */
     @Override
-    public Set<String> getPipeline() {
+    public List<String> getPipeline() {
         return pipleline;
     }
+
+
 }
